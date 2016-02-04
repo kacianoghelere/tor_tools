416c65f24e6a6fb0d31290f71db9e9a289b782fc
mohawkeagle <themohawkeagle@gmail.com>
2016-02-04 21:15:41 -0200

    Finish user edit, update, index, and destroy actions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,7 +2,7 @@ module ApplicationHelper
 	
 	# Returns the full title on a per-page basis.
 	def full_title(page_title = '')
-		base_title = "TOR - Tools"
+		base_title = "The One Ring Tools"
 		if page_title.empty?
 			base_title
 		else
