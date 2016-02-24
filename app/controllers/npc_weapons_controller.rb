b839abc2466b66f68565135b9cf3d9648f063459
Kaciano Ghelere <themohawkeagle@gmail.com>
2016-02-24 10:22:38 -0300

    Estrutura principal finalizada, ufa...

--- app/controllers/npc_weapons_controller.rb
+++ app/controllers/npc_weapons_controller.rb
@@ -1,5 +1,6 @@
 class NpcWeaponsController < ApplicationController
 	before_action :set_npc_weapon, only: [:destroy]
+	before_action :logged_in_user, only: [:destroy]
 
 	def destroy
 		@npc_weapon.destroy
@@ -12,5 +13,14 @@ class NpcWeaponsController < ApplicationController
 		# Use callbacks to share common setup or constraints between actions.
 		def set_npc_weapon
 			@npc_weapon = NpcWeapon.find(params[:id])
+		end
+
+		# Confirms a logged-in user.
+		def logged_in_user
+			unless logged_in?
+				store_location
+				flash.now[:danger] = "Acesse o sistema para efetuar esta operação"
+				redirect_to login_url
+			end
 		end
 end
